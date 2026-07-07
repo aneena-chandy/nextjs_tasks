c32036ff87cc9c6f16e9e09d006af3d6e82f4c84
aneena.chandy <aneena.chandy@techversantinfo.com>
2026-07-07 14:23:22 +0530

    feat: add React fundamentals JSX syntax and element composition examples

---
+++ react-fundamentals/src/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+        <section>
+            <h2>Contact</h2>
+            <p>Email: aneena@example.com</p>
+        </section>
+    );
+}
+
+export default Contact;
---
+++ react-fundamentals/src/Projects.jsx
@@ -0,0 +1,14 @@
+function Projects() {
+    return (
+        <section>
+            <h2>Projects</h2>
+            <ul>
+                <li>React Learning App</li>
+                <li>Portfolio Website</li>
+                <li>Todo Application</li>
+            </ul>
+        </section>
+    );
+}
+
+export default Projects;
